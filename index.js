c364d369e16fa3ae5b06473e3111117cad6b9123
Helena Tenev <htenev14@gmail.com>
2023-02-08 00:01:22 -0500

    got images and names working, trying to work on getting the buttons to click

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const makeupUrl = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
 document.addEventListener("DOMContentLoaded", () => {
-    // const addBtn = document.querySelector("#new-toy-btn");
-    // const toyFormContainer = document.querySelector(".container");
-    //addBtn.addEventListener("click", () => {
-      // hide & seek with the form
     fetch(makeupUrl)
     .then((resp) => resp.json())
-    .then((json) => makeACard(json));
-  function makeACard(json) {
+    .then((json) => makeProductDivs(json))
+    //.then((json) => addClickButton(json));
+  function makeProductDivs(json) {
   json.forEach(element => 
     {const newDiv = document.createElement("div")
     newDiv.id = element.id
+    newDiv.innerText = element.name
+    document.body.appendChild(newDiv)
+    const newInput = document.createElement("input")
+    newInput.setAttribute("type", "button");
+    newInput.setAttribute("value", "Show me more!");
+    newInput.setAttribute("id", `${element.id}-btn`);
+    newInput.setAttribute("class", "buttonclass")
+    newDiv.appendChild(newInput)
     const newImg = document.createElement("img")
     newImg.src = element.image_link 
     newDiv.appendChild(newImg)
-    console.log(newDiv)
-    console.log(element)})
+    })
+    
+
+  }
+  //when domcontentloads, I want to add an Event Listener to each Button
+
+//   const buttonz = document.getElementsByClassName("buttonclass")
+//   function addClickButton(json) {
+//     console.log("this function fires")
+//     //buttonz.addEventListener("click", clickButton())
+//   }
+const buttonz = document.body.getElementsByClassName("buttonclass")
+    console.log(buttonz.length)
+    buttonz.addEventListener("click", clickButton)
+
+  function clickButton(json) {
+    console.log("button clicked")
+    const moreInfoDiv = document.createElement("p")
+    //const buttonNumber = 
+    const prodDescription = json.element.description
+    const prodPrice = json.element.price
+    const prodLink = json.element.product_link
+    moreInfoDiv.innerText = `Description: ${prodDescription} Price: ${prodPrice} Link: ${prodLink} `
+    //document.body.
   }
     });
   //});
